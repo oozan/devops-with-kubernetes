d2f401481bde7f7f2e9a68ea1a4e1ccce7a7c19e
oozan <oozayranci@gmail.com>
2026-07-24 20:19:03 +0300

    Add exercise 1.4 project image

--- exercise-1.2/project/index.js
+++ exercise-1.2/project/index.js
@@ -3,8 +3,16 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from project app\n");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+    <!doctype html>
+    <html>
+      <body>
+        <h1>Hello from project app</h1>
+        <img src="https://picsum.photos/1200" alt="Random image" />
+      </body>
+    </html>
+  `);
 });
 
 server.listen(PORT, () => {
