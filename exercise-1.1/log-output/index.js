bbd42b2aba606c96e3cf26b9a89e652c1a00e45c
oozan <oozayranci@gmail.com>
2026-07-25 21:28:53 +0300

    Add exercise 1.7 log output ingress

--- exercise-1.1/log-output/index.js
+++ exercise-1.1/log-output/index.js
@@ -1,7 +1,20 @@
+const http = require("http");
 const crypto = require("crypto");
 
+const PORT = process.env.PORT || 3000;
 const randomString = crypto.randomUUID();
 
+const getStatus = () => `${new Date().toISOString()}: ${randomString}`;
+
 setInterval(() => {
-  console.log(`${new Date().toISOString()}: ${randomString}`);
+  console.log(getStatus());
 }, 5000);
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(`${getStatus()}\n`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+});
